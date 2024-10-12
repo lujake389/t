beb1bb4f1d2d7f45ade1066073498927c503c620
lujake389 <151537032+lujake389@users.noreply.github.com>
2024-10-13 06:54:42 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
 # Cloudflare API details
-auth_email="YourEmail"                     # Your Cloudflare email
-auth_key="YourGlobalAPI"                   # Your Global API Key
-zone_identifier="YourZondID"               # Zone ID for your domain
-record_name="YourRecord"                   # DNS record to update
-ttl=3600                                   # DNS TTL (in seconds)
-proxy="false"                              # Whether the Cloudflare proxy is enabled (true/false)
+auth_email="sydjwd92@gmail.com"                     # Your Cloudflare email
+auth_key="ab1ca6a961ce2fb2b4f1301f70e9afccdf05c"    # Your Global API Key
+zone_identifier="f2a8fae655d97d06580123f16a7cb7b9"  # Zone ID for your domain
+record_name="yyy.inters.site"                       # DNS record to update
+ttl=3600                                            # DNS TTL (in seconds)
+proxy="false"                                       # Whether the Cloudflare proxy is enabled (true/false)
 
-# Check IP detection function
+# Telegram bot details
+CHATID="5747562905"
+KEY="7422462630:AAEYpTcELKBDiKwztS6F1g9C_TD8HHYmHKM"
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+
+# Function to detect public IP
 detect_public_ip() {
     curl -s https://api.ipify.org
+}
+
+# Function to send notification to Telegram
+send_telegram_notification() {
+    local new_ip="$1"
+    local current_ip="$2"
+    local message="
+    <code>━━━━━━━━━━━━━━━━━━━━━━━━━</code>
+    <b>Cloudflare DDNS Update</b>
+    <code>━━━━━━━━━━━━━━━━━━━━━━━━━</code>
+    <code>Domain   :</code><code>$record_name</code>
+    <code>Old IP   :</code><code>$current_ip</code>
+    <code>New IP   :</code><code>$new_ip</code>
+    <code>━━━━━━━━━━━━━━━━━━━━━━━━━</code>
+    <i>IP address successfully updated on Cloudflare.</i>"
+
+    curl -s --max-time 10 -d "chat_id=$CHATID&disable_web_page_preview=1&text=$message&parse_mode=html" $URL >/dev/null
 }
 
 # Function to update DNS record
@@ -67,12 +89,13 @@ check_and_update_ip() {
     else
         echo "IP has changed. Updating DNS record..."
         update_dns_record "$new_ip" "$record_identifier"
+        # Send Telegram notification
+        send_telegram_notification "$new_ip" "$current_ip"
     fi
 }
 
 # Main loop to check for IP change every 5 minutes (adjust as needed)
 while true; do
     check_and_update_ip
-    sleep 300  # Wait for 5 minutes before checking again
+    sleep 60 # Wait for 5 minutes before checking again 
 done
-
